24f13bff216cffc5639f2a98d4511611cfad6626
Tuanngo-02 <thanhtuanhht0211@gmail.com>
2025-11-26 21:42:34 +0700

    refactor/analysis page cv and jd

--- lib/ui/user/view_models/fileService.dart
+++ lib/ui/user/view_models/fileService.dart
@@ -1,37 +1,52 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 
-import '../widgets/analysisResultPage_screen.dart';
+import 'fileWeb.dart';
 
 Future<Map<String, dynamic>?> sendCvJdFiles({
   required PlatformFile cvFile,
   required PlatformFile jdFile,
 }) async {
   try {
-    var uri = Uri.parse("http://10.0.2.2:8000/analyze/");
+    final isWeb = kIsWeb;
+
+    final uri = Uri.parse(isWeb
+        ? "http://127.0.0.1:8000/analyze/"
+        : "http://10.0.2.2:8000/analyze/");
+
     var request = http.MultipartRequest("POST", uri);
 
-    // Thêm file CV
-    request.files.add(await http.MultipartFile.fromPath(
-      "cv_file",
-      cvFile.path!,
-      filename: cvFile.name,
-    ));
-
-    // Thêm file JD
-    request.files.add(await http.MultipartFile.fromPath(
-      "jd_file",
-      jdFile.path!,
-      filename: jdFile.name,
-    ));
-
-    // Gửi request
+    if (isWeb) {
+      request.files.add(http.MultipartFile.fromBytes(
+        "cv_file",
+        cvFile.bytes!,
+        filename: cvFile.name,
+      ));
+    } else {
+      request.files.add(await http.MultipartFile.fromPath(
+        "cv_file",
+        cvFile.path!,
+        filename: cvFile.name,
+      ));
+    }
+    if (isWeb) {
+      request.files.add(http.MultipartFile.fromBytes(
+        "jd_file",
+        jdFile.bytes!,
+        filename: jdFile.name,
+      ));
+    } else {
+      request.files.add(await http.MultipartFile.fromPath(
+        "jd_file",
+        jdFile.path!,
+        filename: jdFile.name,
+      ));
+    }
     var response = await request.send();
 
     if (response.statusCode == 200) {
@@ -43,28 +58,27 @@ Future<Map<String, dynamic>?> sendCvJdFiles({
       return null;
     }
   } catch (e) {
-    print("Exception sending files: $e");
+    print("Upload Exception: $e");
     return null;
   }
 }
-Future<void> downloadPdf(String fullPath) async {
+Future<void> downloadPdfMobile(String fullPath) async {
   String fileName = fullPath.split(r"\").last;
- print(fileName);
   final url = "http://10.0.2.2:8000/download/$fileName";
-  print(url);
-  try {
-    final response = await http.get(Uri.parse(url));
 
-    if (response.statusCode == 200) {
-      Directory tempDir = await getTemporaryDirectory();
-      String tempPath = '${tempDir.path}/$fileName';
-      File file = File(tempPath);
-      await file.writeAsBytes(response.bodyBytes);
-      await OpenFile.open(file.path);
-    } else {
-      print("Lỗi khi tải file: ${response.statusCode}");
-    }
-  } catch (e) {
-    print("Exception khi tải file: $e");
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    Directory dir = await getTemporaryDirectory();
+    File file = File('${dir.path}/$fileName');
+    await file.writeAsBytes(response.bodyBytes);
+    await OpenFile.open(file.path);
+  }
+}
+Future<void> downloadPdf(String fullPath) async {
+  if (kIsWeb) {
+    await downloadPdfWeb(fullPath);
+  } else {
+    await downloadPdfMobile(fullPath);
   }
 }
---
+++ lib/ui/user/view_models/fileWeb.dart
@@ -0,0 +1,11 @@
+import 'package:web/web.dart' as web;
+
+Future<void> downloadPdfWeb(String fullPath) async {
+  String fileName = fullPath.split(r"\").last;
+  final url = "http://127.0.0.1:8000/download/$fileName";
+
+  final anchor = web.HTMLAnchorElement()
+    ..href = url
+    ..download = fileName
+    ..click();
+}
